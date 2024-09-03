8d7344712de63a0fda018e09d78f2bf25397147e
sickbeoi <saadatullah21@gmail.com>
2024-09-03 16:12:37 +0500

    Setup server.js

--- server.js
+++ server.js
@@ -1,29 +1,70 @@
+// Import necessary modules
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
-const connectDb = require('./config/database');
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
 
-require('dotenv').config();
+
+// Candidate-related routes
+const candidateRegisterRoutes = require('./routes/CandidateRoutes/registerCandidate');
+const candidateProfileRoutes = require('./routes/CandidateRoutes/profileCompletion');
+const returnCandidateRoutes = require('./routes/CandidateRoutes/returnCandidate');
+const loginCandidateRoutes = require('./routes/CandidateRoutes/loginCandidate');
+const adminRoutes = require('./routes/AdminRoutes/adminRoutes');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an instance of Express
 const app = express();
-app.use(cors({ origin: '*' }))
 
-connectDb();
-
-//body-parser
+// Middleware setup
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
+// Candidate routes
+app.use('/api/candidates/register', (req, res, next) => {
+    console.log('Received request at /api/candidates/register');
+    next();
+}, candidateRegisterRoutes);
 
-// authRoutes
-app.use('/api/auth', authRoutes)
+app.use('/api/candidates/profile', (req, res, next) => {
+    console.log('Received request at /api/candidates/profile');
+    next();
+}, candidateProfileRoutes);
 
-//userRoutes middleware
-app.use('/api/user', userRoutes);
+app.use('/api/candidates/get', (req, res, next) => {
+    console.log('Received request at /api/candidates/get');
+    next();
+}, returnCandidateRoutes);
 
-app.get('/', (req, res) => {
-    res.json({ message: "Server Live" })
-})
+app.use('/api/candidate/login', (req, res, next) => {
+    console.log('Received request at /api/candidate/login');
+    next();
+}, loginCandidateRoutes);
 
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`Listening to ${process.env.PORT}`)
-})
+app.use('/api/admin',(req, res, next)=>{
+    console.log('Received request at /api/admin');
+    next();
+}, adminRoutes);
+
+// Connect to MongoDB
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start the server
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+    });
+
+module.exports = app;
