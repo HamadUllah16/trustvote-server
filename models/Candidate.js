7a0c056f28a46839582412db220ae3dbf35d657e
Hamad Ullah <hamadullah16@gmail.com>
2024-09-20 19:10:33 +0500

    Update Candidate.js

--- models/Candidate.js
+++ models/Candidate.js
@@ -1,6 +1,10 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const CandidateSchema = new mongoose.Schema({
+    role: {
+        type: String,
+        required: false
+    },
     firstName: {
         type: String,
         unique: false,
@@ -24,7 +28,7 @@ const CandidateSchema = new mongoose.Schema({
         type: String,
         required: false,
     },
-    cnic: {
+    cnicNumber: {
         type: String,
         required: false,
     },
@@ -40,8 +44,79 @@ const CandidateSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    gender: {
+        type: String,
+        required: false
+    },
+    constituencyType: {
+        type: String,
+        required: false
+    },
+    constituency: {
+        type: String,
+        required: false
+    },
+    partyAffiliation: {
+        type: String,
+        required: false
+    },
+    manifesto: {
+        type: String,
+        required: false
+    },
+    educationalCertificates: {
+        type: String,
+        required: false
+    },
+    assetDeclaration: {
+        type: String,
+        required: false
+    },
+    codeOfConduct: {
+        type: Boolean,
+        required: false
+    },
+
     profileCompletion: {
+        type: Boolean,
+        required: false
+    },
+    approved: {
         type: Boolean,
         required: false
     }
 })
+
+CandidateSchema.pre('save', function (next) {
+    const candidate = this;
+
+    // Check if all required fields for profile completion are present
+    if (
+        candidate.role &&
+        candidate.firstName &&
+        candidate.lastName &&
+        candidate.email &&
+        candidate.phone &&
+        candidate.password &&
+        candidate.cnicNumber &&
+        candidate.dateOfBirth &&
+        candidate.gender &&
+        candidate.constituencyType &&
+        candidate.constituency &&
+        candidate.partyAffiliation &&
+        candidate.manifesto &&
+        candidate.cnicFront &&
+        candidate.cnicBack &&
+        candidate.educationalCertificates &&
+        candidate.assetDeclaration &&
+        candidate.codeOfConduct
+    ) {
+        candidate.profileCompletion = true;
+    } else {
+        candidate.profileCompletion = false;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Candidate', CandidateSchema)
