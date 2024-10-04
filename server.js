ca46c67ee47f900db3c17cb4758ffd36a030cf95
Hamad <hamadullah16@gmail.com>
2024-10-04 18:59:02 +0500

    cors issue fix

--- server.js
+++ server.js
@@ -24,8 +24,19 @@ connectDb()
 
 // Middleware setup
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(morgan('dev'));
+const allowedOrigins = [process.env.CLIENT_URL];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 app.use(express.json({ limit: '20mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
