3c6126aed3972b8fce8244181bbfcd0ab798c3fe
Hamad <hamadullah16@gmail.com>
2024-11-11 01:42:42 +0500

    fixing production

--- server.js
+++ server.js
@@ -17,81 +17,76 @@ const electionSessionRoutes = require('./routes/electionSessionRoutes');
 const multer = require('./middlewares/multer');
 const { fileUpload } = require('./utils/cloudinary');
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Create an instance of Express
 const app = express();
 
-// Connect to MongoDB
-connectDb().then((msg) => {
-    // Middleware setup
-    app.use(express.json());
-    app.use(morgan('dev'));
-    const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3001'];
+connectDb()
 
-    app.use(cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true,
-    }));
+// Middleware setup
+app.use(express.json());
+app.use(morgan('dev'));
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3001'];
 
-    app.use(express.json({ limit: '20mb' }))
-    app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-    //create default admin if not exists
-    createAdmin();
-
-    // Authentication routes
-    app.use('/api/auth', authRoutes)
-    // Candidate routes
-    app.use('/api/candidate', candidateRoutes)
-    // User routes
-    app.use('/api/user', userRoutes)
-    // Admin routes
-    app.use('/api/admin', adminRoutes)
-    // Constituency routes
-    app.use('/api/constituency', constituencyRoutes)
-    // Provincial Constituencies
-    app.use('/api/provincial-constituencies', provincialConstituencyRoutes)
-    // Election Session
-    app.use('/api/election-session', electionSessionRoutes);
-
-    app.post('/upload', multer.single('test'), async (req, res) => {
-        console.log(req.body)
-        try {
-            if (!req.file) {
-                console.error('No file attached.')
-                return res.status(400).send('No file uploaded.')
-            }
-            const fileType = req.file.mimetype.split('/')[0];
-            const cloudinaryUrl = await fileUpload(req.file.buffer, fileType);
-
-            res.send({ url: cloudinaryUrl });
-        } catch (error) {
-            res.status(500).json({ message: 'Internal Server Error.' })
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
-    })
+    },
+    credentials: true,
+}));
 
-    // server setup
-    const PORT = process.env.PORT || 3000;
+app.use(express.json({ limit: '20mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-    app.listen((PORT), () => {
-        console.log(`Listening to ${PORT}`)
-    })
+//create default admin if not exists
+createAdmin();
 
-    app.get('/', (req, res) => {
-        console.log('Server up!')
-        res.status(200).json({ message: 'Server up', database: msg })
-    })
-}).catch((e) => {
-    console.log(e);
-    return e
+// Authentication routes
+app.use('/api/auth', authRoutes)
+// Candidate routes
+app.use('/api/candidate', candidateRoutes)
+// User routes
+app.use('/api/user', userRoutes)
+// Admin routes
+app.use('/api/admin', adminRoutes)
+// Constituency routes
+app.use('/api/constituency', constituencyRoutes)
+// Provincial Constituencies
+app.use('/api/provincial-constituencies', provincialConstituencyRoutes)
+// Election Session
+app.use('/api/election-session', electionSessionRoutes);
+
+app.post('/upload', multer.single('test'), async (req, res) => {
+    console.log(req.body)
+    try {
+        if (!req.file) {
+            console.error('No file attached.')
+            return res.status(400).send('No file uploaded.')
+        }
+        const fileType = req.file.mimetype.split('/')[0];
+        const cloudinaryUrl = await fileUpload(req.file.buffer, fileType);
+
+        res.send({ url: cloudinaryUrl });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error.' })
+    }
 })
+
+// server setup
+const PORT = process.env.PORT || 3000;
+
+app.listen((PORT), () => {
+    console.log(`Listening to ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    console.log('Server up!')
+    res.status(200).json({ message: 'Server up', database: msg })
+})
+
 
 module.exports = app;
